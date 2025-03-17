fc1d59c474e7ce55e34f508b26e6f7614e81f4f6
Daniel Alejandro <danielalejandroroseroortiz80@gmail.com>
2025-03-16 21:57:44 -0500

    quitar header

--- config/db_config.php
+++ config/db_config.php
@@ -1,9 +1,16 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '2212');
-define('DB_DATABASE', 'IMS_invsys');
 
+require_once '../vendor/autoload.php';
 
+// Cargar variables de entorno
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+define('DB_SERVER', $_ENV['DB_SERVER']);
+define('DB_USERNAME', $_ENV['DB_USERNAME']);
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_DATABASE', $_ENV['DB_DATABASE']);
+
+date_default_timezone_set($_ENV['TIMEZONE']); 
 
 ?>
--- config/db_connect.php
+++ config/db_connect.php
@@ -1,6 +1,5 @@
 <?php
 
-date_default_timezone_set('America/Bogota'); // Configurar la zona horaria global
 
 require_once 'db_config.php';
 
@@ -8,5 +7,9 @@ $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 if ($conn->connect_error) {
     die("Conexion fallida: " . $conn->connect_error);
-}
+} 
+
+
 ?>
+
+
