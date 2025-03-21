28226fd03904093a31f8245213c5f9bb9835eac5
Daniel Alejandro <danielalejandroroseroortiz80@gmail.com>
2025-03-20 23:49:11 -0500

    db actualizada

--- config/IMS_invsys (2).sql
+++ config/IMS_invsys (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 16-03-2025 a las 17:10:39
+-- Tiempo de generación: 21-03-2025 a las 04:48:32
 -- Versión del servidor: 11.7.2-MariaDB
 -- Versión de PHP: 8.4.5
 
@@ -213,6 +213,18 @@ CREATE TABLE `imagenes_productos` (
   `ruta_imagen` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `imagenes_productos`
+--
+
+INSERT INTO `imagenes_productos` (`id_imagen`, `id_producto`, `nombre_imagen`, `ruta_imagen`) VALUES
+(1, 3, '8da4f437a948676e5d1666efb4280ab8.png', '../uploads/imagenes/productos/8da4f437a948676e5d1666efb4280ab8.png'),
+(2, 3, 'd1e74abbda52a9897b73388b19fc633d.png', '../uploads/imagenes/productos/d1e74abbda52a9897b73388b19fc633d.png'),
+(3, 2, '995a928c4d5f5e89068d68bba1c4b69b.png', '../uploads/imagenes/productos/995a928c4d5f5e89068d68bba1c4b69b.png'),
+(4, 7, '06d20e2f90873295da0e1f288472ecb8a0e3297cf238a2be8413a496f7a60b5a.png', '../uploads/imagenes/productos/06d20e2f90873295da0e1f288472ecb8a0e3297cf238a2be8413a496f7a60b5a.png'),
+(5, 7, '2ba94dfdcca12b5f2d2ce514d834fbb7b87ddc56b02d4a94ed151085424d857b.png', '../uploads/imagenes/productos/2ba94dfdcca12b5f2d2ce514d834fbb7b87ddc56b02d4a94ed151085424d857b.png'),
+(6, 7, '5049dee7036bb4b9ccb2f5e70e6168370e25db091dc50d075f9b409a63077ec4.png', '../uploads/imagenes/productos/5049dee7036bb4b9ccb2f5e70e6168370e25db091dc50d075f9b409a63077ec4.png');
+
 -- --------------------------------------------------------
 
 --
@@ -225,6 +237,13 @@ CREATE TABLE `imagenes_usuarios` (
   `nombre_imagen` varchar(255) NOT NULL,
   `ruta_imagen` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `imagenes_usuarios`
+--
+
+INSERT INTO `imagenes_usuarios` (`id_imagen`, `id_usuario`, `nombre_imagen`, `ruta_imagen`) VALUES
+(1, 47, '73f8f5bfa6361d9f51541378b8003127912740d16ad0518667e7d9b6aa650b3a.png', '../uploads/imagenes/usuarios/73f8f5bfa6361d9f51541378b8003127912740d16ad0518667e7d9b6aa650b3a.png');
 
 -- --------------------------------------------------------
 
@@ -250,7 +269,9 @@ CREATE TABLE `movimientos_stock` (
 INSERT INTO `movimientos_stock` (`id_movimiento`, `id_producto`, `id_almacen_origen`, `id_almacen_destino`, `tipo_movimiento`, `cantidad`, `fecha_movimiento`, `id_usuario`) VALUES
 (1, 1, 2, 1, 'entrada', 50, '2025-03-12 22:05:59', 47),
 (2, 2, 1, 2, 'entrada', 100, '2025-03-12 22:05:59', 44),
-(3, 3, 1, 2, 'transferencia', 10, '2025-03-12 22:05:59', 47);
+(3, 3, 1, 2, 'transferencia', 10, '2025-03-12 22:05:59', 47),
+(5, 3, 1, 2, 'transferencia', 50, '2025-03-16 17:36:30', 55),
+(7, 3, 2, 1, 'transferencia', 33, '2025-03-17 01:47:44', 47);
 
 -- --------------------------------------------------------
 
@@ -271,21 +292,22 @@ CREATE TABLE `productos` (
   `stock_maximo` int(11) DEFAULT 0,
   `id_categoria` int(11) DEFAULT NULL,
   `fecha_creacion` timestamp NULL DEFAULT current_timestamp(),
-  `fecha_actualizacion` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `fecha_actualizacion` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `id_proveedor` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id_producto`, `nombre`, `codigo`, `sku`, `descripcion`, `precio_compra`, `precio_venta`, `id_unidad_medida`, `stock_minimo`, `stock_maximo`, `id_categoria`, `fecha_creacion`, `fecha_actualizacion`) VALUES
-(1, 'Smartphone X', 'PROD001', 'SKU12345', 'Teléfono inteligente última generación', 800000.00, 1200000.00, 2, 5, 100, 1, '2025-03-12 22:05:59', '2025-03-15 16:36:30'),
-(2, 'Camisa Algodón', 'PROD002', 'SKU54321', 'Camisa de algodón talla M', 50000.00, 85000.00, 1, 20, 200, 2, '2025-03-12 22:05:59', '2025-03-12 22:05:59'),
-(3, 'Juego de Sábanas', 'PROD003', 'SKU67890', 'Sábanas de algodón egipcio', 120000.00, 180000.00, 1, 15, 150, 3, '2025-03-12 22:05:59', '2025-03-12 22:05:59'),
-(7, 'ProductoPrueba', 'PROD999', 'SKU99999', 'Este es un producto de prueba', 1000.50, 1500.75, 1, 5, 50, 1, '2025-03-13 03:12:01', '2025-03-13 03:12:01'),
-(12, 'ProductoPrueba', 'PROD9999', 'SKU999999', 'Este es un producto de prueba', 1000.50, 1500.75, 1, 5, 50, 1, '2025-03-13 03:20:41', '2025-03-13 03:20:41'),
-(13, 'ProductoPrueba', 'PROD9999999', 'SKU9999999', 'Este es un producto de prueba', 1000.50, 1500.75, 1, 5, 50, 1, '2025-03-13 03:39:21', '2025-03-13 03:39:21'),
-(14, 'ProductoActualizado', 'PROD000014', 'SKU000014', 'Este producto fue editado de prueba', 1200.50, 1700.75, 1, 5, 50, 1, '2025-03-13 19:59:54', '2025-03-13 20:07:33');
+INSERT INTO `productos` (`id_producto`, `nombre`, `codigo`, `sku`, `descripcion`, `precio_compra`, `precio_venta`, `id_unidad_medida`, `stock_minimo`, `stock_maximo`, `id_categoria`, `fecha_creacion`, `fecha_actualizacion`, `id_proveedor`) VALUES
+(1, 'Smartphone X', 'PROD001', 'SKU12345', 'Teléfono inteligente última generación', 800000.00, 1200000.00, 2, 5, 100, 1, '2025-03-12 22:05:59', '2025-03-18 05:28:15', 2),
+(2, 'Camisa Algodón', 'PROD002', 'SKU54321', 'Camisa de algodón talla M', 50000.00, 85000.00, 1, 20, 200, 2, '2025-03-12 22:05:59', '2025-03-18 05:28:11', 2),
+(3, 'Juego de Sábanas', 'PROD003', 'SKU67890', 'Sábanas de algodón egipcio', 120000.00, 180000.00, 1, 15, 150, 3, '2025-03-12 22:05:59', '2025-03-18 05:28:08', 1),
+(7, 'ProductoPrueba', 'PROD999', 'SKU99999', 'Este es un producto de prueba', 1000.50, 1500.75, 1, 5, 50, 1, '2025-03-13 03:12:01', '2025-03-18 05:28:05', 2),
+(12, 'ProductoPrueba', 'PROD9999', 'SKU999999', 'Este es un producto de prueba', 1000.50, 1500.75, 1, 5, 50, 1, '2025-03-13 03:20:41', '2025-03-18 05:28:01', 2),
+(13, 'ProductoPrueba', 'PROD9999999', 'SKU9999999', 'Este es un producto de prueba', 1000.50, 1500.75, 1, 5, 50, 1, '2025-03-13 03:39:21', '2025-03-18 05:27:56', 1),
+(14, 'ProductoActualizado', 'PROD000014', 'SKU000014', 'Este producto fue editado de prueba', 1200.50, 1700.75, 1, 5, 50, 1, '2025-03-13 19:59:54', '2025-03-18 05:25:37', 1);
 
 -- --------------------------------------------------------
 
@@ -329,9 +351,12 @@ CREATE TABLE `stock_almacen` (
 
 INSERT INTO `stock_almacen` (`id_stock`, `id_almacen`, `id_producto`, `cantidad_disponible`) VALUES
 (1, 1, 12, 1),
-(2, 1, 3, 111),
+(2, 1, 3, 11111),
 (3, 2, 2, 111),
-(4, 2, 3, 100);
+(4, 2, 3, 67),
+(6, 2, 3, 17),
+(7, 2, 1, 1000),
+(8, 1, 3, 11111);
 
 -- --------------------------------------------------------
 
@@ -379,14 +404,11 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `nombre`, `email`, `password`, `status`, `last_login`, `nivel_usuario`, `nombreUsuario`, `token_recuperacion`, `expira_token`) VALUES
-(42, 'NudevoNombre', 'usuario_prueba@example.com', '$2y$12$1xlEgpJPsDDmigAxBK1VqurC8FDzMCHdjTOS44ej3fHLorYy8OjOi', 1, '2025-03-09 23:26:57', 1, 'nduevo_usuario', NULL, NULL),
 (44, 'ddadwqdwqddni', 'usuario_nuevo@example.com', '$2y$12$C5FqLc/lNPSW8uC0YtozcuQL/QOY/fkYIC8nqrXM7d/YwA0QY3K6W', 1, '2025-03-09 23:39:28', 1, 'dwdwdadndiel', NULL, NULL),
-(45, 'UsuarioNuevo', 'udsuadwqdrio_nuevo@example.com', '$2y$12$1nBRJCz2LmZbh3W3aeJvl.kn7pS8hHLerONGFhun0BWcjBa82lkou', 1, '2025-03-09 23:41:40', 1, 'usduario_nuevo', NULL, NULL),
-(46, 'UsuarioNuevo', 'uddsuadwqdrio_nuevo@example.com', '$2y$12$rsyMG/ne7vtXTV2Tf6gLGeyqNDr2cxhQekzMERzrdERnnOOXiE87G', 1, '2025-03-10 04:24:18', 1, 'udddddsduario_nuevo', NULL, NULL),
-(47, 'daniel alejandro', 'danielalejandroroseroortiz80@gmail.com', '$2y$12$2vEg5sPHM9WGW.p6zBa0yuB2OioDBDPtf2RziHG25Gke3ptVY.ToG', 1, '2025-03-11 02:54:17', 1, 'danielalejandrorosero', NULL, NULL),
-(53, 'daniel alejandro', 'danielalejandroroseroddortiz80@gmail.com', '$2y$12$LLOeUvPVCMKOSiLGsbjlfOFVs1f/0dJZZTn4GT0Xp50oAn0SjwA3S', 1, '2025-03-11 19:19:58', 1, 'admin', NULL, NULL),
-(54, 'Usuario Prueba', 'test@example.com', '$2y$12$1xlEgpJPsDDmigAxBK1VqurC8FDzMCHdjTOS44ej3fHLorYy8OjOi', 1, NULL, 2, 'test_user', NULL, NULL),
-(55, 'daniel alejandro', 'danieladdwqdlejandroroseroortiz80@gmail.com', '$2y$12$t1EXcGjI1rNPTRulqnCPceQwAuUpcPK4ZO8zTFG0LCDwnOH7WTY56', 1, '2025-03-12 19:14:55', 1, 'adminn', NULL, NULL);
+(47, 'daniel alejandro', 'danielalejandroroseroortiz80@gmail.com', '$2y$12$UNAMheynOx8VC8OTh7xpxuUDbmT1IuhhOd9ZMgYUAQMbuZ1x6fYcS', 1, '2025-03-11 02:54:17', 1, 'danielalejandrorosero', '7dcb2bb53274322fcbcb5d187b56148ac871db6c9c6c8e49d49ec0fa5c2869e68bbb143923a5799e2a780ce719339b5fd5f3', '2025-03-20 23:35:00'),
+(55, 'daniel alejandro', 'danieladdwqdlejandroroseroortiz80@gmail.com', '$2y$12$t1EXcGjI1rNPTRulqnCPceQwAuUpcPK4ZO8zTFG0LCDwnOH7WTY56', 1, '2025-03-12 19:14:55', 1, 'adminn', NULL, NULL),
+(65, 'administradro', 'daniewqdwqdqwdlalejandroroseroortiz80@gmail.com', '$2y$12$0RJrEndH5Z57f/tH3gseWeyKx6HEpTn4xFws/ydW4MDgnBQAbYRjW', 1, '2025-03-20 19:23:32', 1, 'rootaaaqaaaa', NULL, NULL),
+(66, 'el admin', 'daniwqdqddwqdwqdqwdwqelalejandroroseroortiz80@gmail.com', '$2y$12$TH86kDuY.GowvhzYGG0LLuu3JQ2twSlL.tlpjze9ATy6FB0IvWyIy', 1, '2025-03-20 23:39:45', 1, 'admins de administrador', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -502,7 +524,8 @@ ALTER TABLE `productos`
   ADD UNIQUE KEY `codigo` (`codigo`),
   ADD UNIQUE KEY `unique_sku` (`sku`),
   ADD KEY `id_categoria` (`id_categoria`),
-  ADD KEY `fk_unidad_medida` (`id_unidad_medida`);
+  ADD KEY `fk_unidad_medida` (`id_unidad_medida`),
+  ADD KEY `fk_productos_proveedores` (`id_proveedor`);
 
 --
 -- Indices de la tabla `proveedores`
@@ -598,19 +621,19 @@ ALTER TABLE `grupos`
 -- AUTO_INCREMENT de la tabla `imagenes_productos`
 --
 ALTER TABLE `imagenes_productos`
-  MODIFY `id_imagen` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_imagen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `imagenes_usuarios`
 --
 ALTER TABLE `imagenes_usuarios`
-  MODIFY `id_imagen` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_imagen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `movimientos_stock`
 --
 ALTER TABLE `movimientos_stock`
-  MODIFY `id_movimiento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_movimiento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
@@ -628,7 +651,7 @@ ALTER TABLE `proveedores`
 -- AUTO_INCREMENT de la tabla `stock_almacen`
 --
 ALTER TABLE `stock_almacen`
-  MODIFY `id_stock` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_stock` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `unidades_medida`
@@ -640,7 +663,7 @@ ALTER TABLE `unidades_medida`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas`
@@ -695,7 +718,7 @@ ALTER TABLE `imagenes_usuarios`
 -- Filtros para la tabla `movimientos_stock`
 --
 ALTER TABLE `movimientos_stock`
-  ADD CONSTRAINT `fk_movimientos_stock_usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `fk_movimientos_stock_usuarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE,
   ADD CONSTRAINT `movimientos_stock_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`),
   ADD CONSTRAINT `movimientos_stock_ibfk_2` FOREIGN KEY (`id_almacen_origen`) REFERENCES `almacenes` (`id_almacen`),
   ADD CONSTRAINT `movimientos_stock_ibfk_3` FOREIGN KEY (`id_almacen_destino`) REFERENCES `almacenes` (`id_almacen`);
@@ -704,6 +727,7 @@ ALTER TABLE `movimientos_stock`
 -- Filtros para la tabla `productos`
 --
 ALTER TABLE `productos`
+  ADD CONSTRAINT `fk_productos_proveedores` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedores` (`id_proveedor`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_productos_unidad` FOREIGN KEY (`id_unidad_medida`) REFERENCES `unidades_medida` (`id_unidad`),
   ADD CONSTRAINT `fk_unidad_medida` FOREIGN KEY (`id_unidad_medida`) REFERENCES `unidades_medida` (`id_unidad`),
   ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`) ON DELETE SET NULL;
